8f627e32c99fc82f485cc508b5f429dee5eab67a
Bharat Verma <bharatverma@Bharats-MacBook-Pro.local>
2020-05-06 23:05:25 +0530

    changes for quantity

--- app/Http/Controllers/user/ProductController.php
+++ app/Http/Controllers/user/ProductController.php
@@ -39,6 +39,8 @@ class ProductController extends Controller
     {
     	$addToCart = UserHelper::updateCart($request); //add product to cart
         if ($addToCart['status']) {
+            if($request->quantity == -1)
+                return return['status'=>true];
             $refreshCart = $this->refreshCart($addToCart['session_cart']);
             return $refreshCart;
         } else {
